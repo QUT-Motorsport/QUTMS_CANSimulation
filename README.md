d70ad312ff3db6ca9e768148c09080ccee9e44ca
Smuggler@Shelob <smugglersbc@gmail.com>
2020-02-25 12:45:03 +1000

    Added part of the docs to SPi commands, modes, Tx and part of Rx

---
+++ README.md
@@ -0,0 +1,6 @@
+# MCP2515 and 2517 CAN Messaging implementation
+The code is meant to bring an understanding and set foundation for the first usage of Controller Area Network (CAN) bus protocol to exchange data. This particular one makes an example on a Teensy 2.0 and Rapberry Pi.
+
+# MCP2515
+Impments the CAN2.0 at 1 Megabit/second.
+High speed SPI Interface (10MHz)
--- libs/mcp2515_defs.h
+++ libs/mcp2515_defs.h
@@ -3,51 +3,130 @@
 
 #include "macros.h"
 
-#define CMD_RESET        0xC0
-#define CMD_READ         0x03
-#define CMD_WRITE        0x02
-#define CMD_RTS          0x80
-#define CMD_RD_STATUS    0xA0
-#define CMD_BIT_MOD      0x05
 
-/// Number of buffers for send
-#define TXB_CNT      3
-/// Number of Filters
-#define FILTR_CNT    6
+#define TXB_CNT         3       // Number of buffers for send
+#define FILTR_CNT       6       // Number of Filters
+#define ERRATA_REP_CNT	20      // Number of trials after failing to set mode
+/**     SPI Interface commands
+ * The MCP2515 is designed to interface directly with the
+ * Serial Peripheral Interface (SPI) port  and supports
+ * Mode 0,0 and Mode 1,1.
+ * 
+ * Reset interlal register/config mode:     1100 0000   =   0xC0
+ * Read data from register at addr:         0000 0011   =   0x03
+ * Reading a receive buffer:                1001 0nm0
+ * Write data to register at addr:          0000 0010   =   0x02
+ * RTS (Message Request to Send): TXBn      1000 0nnn   =   0x80
+ * Quick poll to read status bits:          1010 0000   =   0xA0
+ * Quick pooling filter match, message type 1011 0000
+ * User to set/clear bits at registers:     0000 0101   =   0x05
+ * 
+*/
+#define CMD_RESET        0xC0   // Reset interlal register/config mode: 1100 0000
+#define CMD_READ         0x03   // Read data from register at addr: 0000 0011
+#define CMD_WRITE        0x02   // Write data to register at addr: 0000 0010
+#define CMD_RTS          0x80   // RTS (Message Request to Send): TXBn  1000 0nnn
+#define CMD_RD_STATUS    0xA0   // Quick poll to read status bits: 1010 0000
+#define CMD_BIT_MOD      0x05   // User to set/clear bits at registers: 0000 0101
 
-/* Tx adress registers */
-#define TXB_BASE  0x30
-#define TXB_DELTA    0x10
-#define TXB0CTRL    TXB_BASE
-#define TXB1CTRL    (TXB0CTRL + TXB_DELTA)
-#define TXB2CTRL    (TXB1CTRL + TXB_DELTA)
+/**     Work Mods
+ * MCP25 operates in 5 modes. Each can be set by 
+ * modefying CANCTRL registers. REQOP[0:2]
+ * 
+ * Normal Operation Mode:        000x xxxx   =   0x00
+ * Sleep Mode:                   001x xxxx   =   0x20
+ * Loopback Mode:                010x xxxx   =   0x40
+ * Listen-Only Mode:             011x xxxx   =   0x60
+ * Sets Configuration Mode:      100x xxxx   =   0x80
+ * * All other values are invalid. At start and after rest
+ * config mode set by default
+ * 
+ * The CLKOUT pin is provided to the system designer
+ * for use as the main system clock or as a clock input
+ * for other devices in the system. MAX 25MHz. Default SC/8.
+ * F_CLCOUT     System Clock/1: 00
+ * F_CLCOUT     System Clock/2: 01
+ * F_CLCOUT     System Clock/4: 10
+ * F_CLCOUT     System Clock/8: 11
+*/
+#define MODE_NORMAL   0x00      // Normal Operation Mode: 000x xxxx
+#define MODE_SLEEP    0x20      // Sleep Mode: 001x xxxx
+#define MODE_LOOP     0x40      // Loopback Mode: 010x xxxx
+#define MODE_LISTEN   0x60      // Listen-Only Mode: 011x xxxx
+#define MODE_CONFIG   0x80      // Sets Configuration Mode: 100x xxxx
+#define MODE_MASK	  (MODE_NORMAL | MODE_SLEEP | MODE_LOOP | MODE_LISTEN | MODE_CONFIG)
+
+/** Bit definition for CANCTRL. Page 60 */
+#define REQOP2		7   // R/W-1: Operation Mode bits
+#define REQOP1		6   // R/W-0: Operation Mode bits
+#define REQOP0		5   // R/W-0: Operation Mode bits
+#define ABAT		4   // R/W-0: Abort All Pending Transmissions
+#define CLKEN		2   // R/W-1: One-Shot Mode - 1:OneTine 0:Reattempt
+#define CLKPRE1		1   // R/W-1: CLKOUT Pin prescaler
+#define CLKPRE0		0   // R/W-1: CLKOUT Pin prescaler
+
+/**     Tx adress registers
+ * TXBnCTRL - control register associated with the message buffer.
+ * It determines the condition under  which the message will be send.
+ * 
+ * Addresses on Page 18: 30h,40h,50h.
+ * TXB_BASE serves as TXB0CTRL, and TXB_Delta is a shift towards next one
+ * 
+ * TXP bits used to transmit message priority
+ * Highest Message priority:            11
+ * High intermediate message priority:  10
+ * Low intermediate message priority:   01
+ * Lowest Message Priority:             00
+*/
+#define TXB_BASE        0x30
+#define TXB_DELTA       0x10
+#define TXB0CTRL        TXB_BASE
+#define TXB1CTRL        (TXB0CTRL + TXB_DELTA)
+#define TXB2CTRL        (TXB1CTRL + TXB_DELTA)
+// TXBn bits
+#define ABTF    _BV(6)  // R-0: Message Aborted Flag bit - 1:Aborted 0:Succes
+#define MLOA    _BV(5)  // R-0: Message Lost Arbitration bit - 1:Lost
+#define TXERR   _BV(4)  // R-0: Transmission Error Detected bit - 1:Bus error during transmission
+#define TXREQ   _BV(3)  // R/W-0: Message Transmit Request bit - 1: Buffer is pending in transmission
+/*Currently unused*/
+#define TXP1    _BV(1)  // R/W-0: Buffer Priority Bit
+#define TXP0    _BV(0)  // R/W-0: Buffer Priority Bit
+
 #define CTRL      0x00
-#define SIDH        0x01
+#define SIDH      0x01
 #define SIDL      0x02
 #define EID8      0x03
 #define EID0      0x04
-#define DLC         0x05
-#define D0          0x06
+#define DLC       0x05
+#define D0        0x06
 
-// TXBn bits
-#define TXREQ    _BV(3)
-#define TXERR    _BV(4)
-#define MLOA    _BV(5)
-#define ABTF    _BV(6)
 
 #define EXIDE    _BV(3)
 #define RTR        _BV(6)
 
-/* Rx */
+/**     Rx address registers
+ * Two full receive buffers with multiple acceptance 
+ * filters for each. There is also a separate Message
+ * Assembly Buffer (MAB) that acts as a third receive
+ * buffer.
+ * 
+ * Addresses on Page 27: 60h.
+ * RXB_BASE serves as RXB0CTRL, and RXB_Delta is a shift towards next one
+ * 
+*/
 #define RXB0CTRL    0x60
-#define RXB_BASE      RXB0CTRL
-#define RXB_DELTA    0x10
+#define RXB_BASE    RXB0CTRL
+#define RXB_DELTA   0x10
 #define RXB1CTRL    (RXB_BASE + RXB_DELTA)
 
-#define RXM1    _BV(6)
-#define RXM0    _BV(5)
-#define BUKT    _BV(2)
-#define RXRTR    _BV(3)
+#define RXM1        _BV(6)  // R/W-0: Receive Buffer Operating bits
+#define RXM0        _BV(5)  // R/W-0: Receive Buffer Operating bits
+#define RXRTR       _BV(3)  // R-0: Received Remote Transfer Request
+#define BUKT        _BV(2)  // R/W-0: Rollover Enable
+/*Currently unused*/
+#define BUKT1       _BV(1)  // R-0: Read-only copy of BUKT bit
+#define FILHIT0     _BV(0)  // R-0: Filter Hit - indicates which acceptance filter enabled reception of message
+
 #define NOFILTR_RX    (RXM1 | RXM0)
 #define EXT_FILTR_RX    RXM1
 #define STD_FILTR_RX    RXM0
@@ -84,16 +163,6 @@
 #define RXF3SIDH  0x10
 #define RXF4SIDH  0x14
 #define RXF5SIDH  0x18
-
-/* Work Mods */
-#define MODE_NORMAL   0x00
-#define MODE_SLEEP    0x20
-#define MODE_LOOP     0x40
-#define MODE_LISTEN   0x60
-#define MODE_CONFIG   0x80
-#define MODE_MASK	  (MODE_NORMAL | MODE_SLEEP | MODE_LOOP | MODE_LISTEN | MODE_CONFIG)
-
-#define ERRATA_REP_CNT	20
 
 /* interrupts */
 #define CANINTE   0x2B
@@ -132,15 +201,6 @@
 #define RX0IE        _BV(0)
 #define RX0IF        _BV(0)
 #define RX0I        _BV(0)
-
-/** \brief	Bit definition for CANCTRL */
-#define REQOP2		7
-#define REQOP1		6
-#define REQOP0		5
-#define ABAT		4
-#define CLKEN		2
-#define CLKPRE1		1
-#define CLKPRE0		0
 
 /** \brief	Bit definition for CNF3 */
 #define WAKFIL		6
